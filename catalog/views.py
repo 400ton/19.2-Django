ce1d0ab854a84bfe3aaea68655befaf0f09c19aa
Tony <king8sky@gmail.com>
2024-05-07 18:35:04 +1100

    update

--- catalog/views.py
+++ catalog/views.py
@@ -10,7 +10,9 @@ def contacts(request):
         name = request.POST.get('name')
         telephone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'You have new message from {name}({telephone}): {message}')
+        with open('feedback.txt', 'w') as file:
+            file.write(f'{name},{telephone}, {message}')
+
     return render(request, 'catalog/contacts.html')
 
 
