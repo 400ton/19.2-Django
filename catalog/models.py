f1bb89611f1c3618d0a5c7de0785638c840fab04
Tony <king8sky@gmail.com>
2024-05-12 22:54:48 +1100

    Updating the project to Lesson 20.1 Working with ORM in Django. Completed the first 3 tasks, started execution of 4: creating and applying migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {"blank": True, "null": True}
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название категории",)
+    description = models.TextField(verbose_name="Описание категории", **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название продукта")
+    description = models.TextField(verbose_name="Описание продукта", **NULLABLE)
+    preview = models.ImageField(upload_to="products/", verbose_name="Фото продукта", **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория", related_name="Products", **NULLABLE)
+    price = models.IntegerField(verbose_name="Цена продукта",**NULLABLE)
+    created_at = models.DateField(verbose_name="Дата создания", **NULLABLE)
+    updated_at = models.DateField(verbose_name="Дата изменения", **NULLABLE)
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Продукт'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Продукты'  # Настройка для наименования набора объектов
+
