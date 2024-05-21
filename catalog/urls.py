30bb3a1dddf5033dd270beac82c80448fcea5b96
Tony <king8sky@gmail.com>
2024-05-22 01:06:52 +1100

    linking a product template to product cards

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from catalog.apps import CatalogConfig
 from catalog.views import home, contacts, catalog, info_product
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from  django.conf.urls.static import static
 from django.urls import path, include
 
-urlpatterns = ([
+urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-               + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT ))
+    path('', include('catalog.urls', namespace='catalog')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
