89090b16b22c18376572750ec45717575b4fe4d9
Tony <king8sky@gmail.com>
2024-06-17 00:31:18 +1100

    changing the menu, developing a form for sending verification messages to the user’s email

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, ResetPassword, email_confirm
 
 app_name = UsersConfig.name
 
@@ -10,5 +10,8 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile')
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('reset-pass/', ResetPassword.as_view(), name='reset_pass'),
+    path('email-confirm/<str:verification_code>/', email_confirm, name='email_confirm'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,11 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+import secrets
+
+from django.conf.global_settings import EMAIL_HOST_USER
+from django.contrib.auth.forms import PasswordResetForm
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView, TemplateView
 
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
@@ -11,6 +17,59 @@ class RegisterView(CreateView):
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
     success_message = 'Вы успешно зарегистрировались'
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        user.set_password(user.password)
+        verification_code = secrets.token_hex(16)
+        user.verification_code = verification_code
+        user.save()
+        host = self.request.get_host()
+        url = f'https://{host}/users/email.confirm/{verification_code}'
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Для подтверждения почты перейдите по ссылке: {url}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        user.save()
+        return super().form_valid(form)
+
+
+def email_confirm(request, verification_code):
+    verification_code = request.POST.get('verification_code')
+    user = get_object_or_404(User, verification_code=verification_code)
+    if user:
+        user.is_active = True
+        user.save()
+        return redirect(reverse_lazy('users:login'))
+    else:
+        return redirect(reverse_lazy('users:register'))
+
+
+class ResetPassword(TemplateView):
+    form_class = PasswordResetForm
+    success_url = reverse_lazy('users:login')
+
+    def get_template_names(self):
+        return 'users/reset_password.html'
+
+    def form_valid(self, form):
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            user = User.objects.get(email=email)
+            if user:
+                password = User.objects.make_random_password(length=8)
+                user.set_password(password)
+                user.save()
+                send_mail(
+                    'Смена пароля',
+                    f'Ваш новый пароль: {password}',
+                    from_email=EMAIL_HOST_USER,
+                    recipient_list=[user.email],
+                )
+            return redirect(reverse("users:login"))
 
 
 class ProfileView(UpdateView):
